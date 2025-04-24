6c91378a9e0e54439ba21264f0f423c150557af7
wangxing <wangxing@nova.net.cn>
2025-04-24 14:51:53 +0800

    bug fix

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import neuralgym as ng
 
 from inpaint_model import InpaintCAModel
 from tensorflow.keras.mixed_precision import set_global_policy
-set_global_policy('mixed_float16')
+#set_global_policy('mixed_float16')
 parser = argparse.ArgumentParser()
 parser.add_argument('--image', default='', type=str,
                     help='The filename of image to be completed.')
